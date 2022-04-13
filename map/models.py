2d6bde109e0d2bc2197f7fbfe0c86fccb05e0400
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-13 23:39:38 +0300

    learn django maps

--- map/models.py
+++ map/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Measurement(models.Model):
+    """Model definition for Measurement."""
+
+    # TODO: Define fields here
+    location = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    distance = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Measurement."""
+
+        verbose_name = 'Measurement'
+        verbose_name_plural = 'Measurements'
+
+    def __str__(self):
+        """Unicode representation of Measurement."""
+        return f"Distance from {self.location} to {self.destination} is {self.distance} km"
+
+    
