fc5588f2a76250bae5f06a3524c9be21fbfb1073
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-14 19:01:59 +0300

    make it presentable

--- map/views.py
+++ map/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import context
 from .models import Measurement
 from .forms import MeasurmentsForm
 from geopy.geocoders import Nominatim
-from .utils import get_geo
+from .utils import get_geo, get_center_coordinates, get_zoom
 from geopy.distance import geodesic
 import folium
 
@@ -16,9 +15,11 @@ import folium
 #                   { 'mapbox_access_token': mapbox_access_token })
 
 def calculate_distance_view(request):
+    distance = None
     obj = get_object_or_404(Measurement, id = 3)
     form = MeasurmentsForm(request.POST or None)
     geolocator = Nominatim(user_agent='map')
+    destination = None
 
     ip = '72.14.207.99'
     country, city, lat, lon = get_geo(ip)
@@ -35,7 +36,7 @@ def calculate_distance_view(request):
     pointA = (l_lat, l_lon)
 
     # innitial folium map
-    m = folium.Map(width=800, height=500, location=pointA)
+    m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon), zoom_start=8)
 
     #location marker
     folium.Marker([l_lat, l_lon], tooltip='Click here for more', popup=city['city'],
@@ -57,8 +58,13 @@ def calculate_distance_view(request):
         # distance calculation
         distance = round(geodesic(pointA, pointB).km, 2)
 
+        # draw locations
+
+        line = folium.PolyLine(locations=[pointA, pointB], weight = 3, color='blue')
+        m.add_child(line)
+
         #folium map modification
-        m = folium.Map(width=800, height=500, location=pointA)
+        m = folium.Map(width=800, height=500, location = get_center_coordinates(l_lat, l_lon, d_lat, d_lon), zoom_start=get_zoom(distance))
 
         #destination marker
         folium.Marker([d_lat, d_lon], tooltip='Click here for more', popup=destination,
@@ -71,6 +77,7 @@ def calculate_distance_view(request):
 
     m = m._repr_html_() #html representation
 
-    context = {'distance': obj, 'form': form, 'map': m, }
+
+    context = {'distance': distance, 'form': form, 'map': m, 'destination': destination, }
 
     return render(request, 'map/main.html', context)
